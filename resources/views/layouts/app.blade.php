e35301b438787e434097a1f9f9fe9c00c8246f6f
miezah8 <nurulhamizah.ma@gmail.com>
2025-05-26 02:57:50 +0800

    Update app.blade.php
    bladify update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet" />
     {{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js" crossorigin="anonymous"></script> --}}
-    
+
 
     {{-- CSS --}}
     <link href="{{ asset('assets/css/soft-ui-dashboard.css') }}" rel="stylesheet" />
@@ -35,6 +35,28 @@
         </div>
     </main>
 
+    <!--   Core JS Files   -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../../assets/js/core/popper.min.js"></script>
+    <script src="../../assets/js/core/bootstrap.min.js"></script>
+    <script src="../../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="../../assets/js/plugins/datatables.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../../assets/js/soft-ui-dashboard.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            if (navigator.platform.indexOf('Win') > -1) {
+                var $scrollbar = $('#sidenav-scrollbar');
+                if ($scrollbar.length) {
+                    var options = {
+                        damping: '0.5'
+                    };
+                    Scrollbar.init($scrollbar[0], options);
+                }
+            }
+        });
+    </script>
     @stack('scripts')
 </body>
 
