2492236412264f906df5daa77f695ef77077d22b
miezah8 <nurulhamizah.ma@gmail.com>
2025-05-26 06:14:58 +0800

    house clean

--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Dashboard')
+@section('title', 'School Module')
 
 @section('content')
     <div class="card">
@@ -21,212 +21,44 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Tiger Nixon</td>
-                        <td class="text-sm font-weight-normal">
-                            System Architect
-                        </td>
-                        <td class="text-sm font-weight-normal">Edinburgh</td>
-                        <td class="text-sm font-weight-normal">61</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Garrett Winters
-                        </td>
-                        <td class="text-sm font-weight-normal">Accountant</td>
-                        <td class="text-sm font-weight-normal">Tokyo</td>
-                        <td class="text-sm font-weight-normal">63</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Ashton Cox</td>
-                        <td class="text-sm font-weight-normal">
-                            Junior Technical Author
-                        </td>
-                        <td class="text-sm font-weight-normal">San Francisco</td>
-                        <td class="text-sm font-weight-normal">66</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Cedric Kelly</td>
-                        <td class="text-sm font-weight-normal">
-                            Senior Javascript Developer
-                        </td>
-                        <td class="text-sm font-weight-normal">Edinburgh</td>
-                        <td class="text-sm font-weight-normal">22</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Airi Satou</td>
-                        <td class="text-sm font-weight-normal">Accountant</td>
-                        <td class="text-sm font-weight-normal">Tokyo</td>
-                        <td class="text-sm font-weight-normal">33</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Brielle Williamson
-                        </td>
-                        <td class="text-sm font-weight-normal">
-                            Integration Specialist
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">61</td>
-                     
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Herrod Chandler
-                        </td>
-                        <td class="text-sm font-weight-normal">
-                            Sales Assistant
-                        </td>
-                        <td class="text-sm font-weight-normal">San Francisco</td>
-                        <td class="text-sm font-weight-normal">59</td>
-                       
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Rhona Davidson</td>
-                        <td class="text-sm font-weight-normal">
-                            Integration Specialist
-                        </td>
-                        <td class="text-sm font-weight-normal">Tokyo</td>
-                        <td class="text-sm font-weight-normal">55</td>
-                    
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Colleen Hurst</td>
-                        <td class="text-sm font-weight-normal">
-                            Javascript Developer
-                        </td>
-                        <td class="text-sm font-weight-normal">San Francisco</td>
-                        <td class="text-sm font-weight-normal">39</td>
-                  
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Sonya Frost</td>
-                        <td class="text-sm font-weight-normal">
-                            Software Engineer
-                        </td>
-                        <td class="text-sm font-weight-normal">Edinburgh</td>
-                        <td class="text-sm font-weight-normal">23</td>
-             
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Jena Gaines</td>
-                        <td class="text-sm font-weight-normal">Office Manager</td>
-                        <td class="text-sm font-weight-normal">London</td>
-                        <td class="text-sm font-weight-normal">30</td>
-         
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Quinn Flynn</td>
-                        <td class="text-sm font-weight-normal">Support Lead</td>
-                        <td class="text-sm font-weight-normal">Edinburgh</td>
-                        <td class="text-sm font-weight-normal">22</td>
-     
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Charde Marshall
-                        </td>
-                        <td class="text-sm font-weight-normal">
-                            Regional Director
-                        </td>
-                        <td class="text-sm font-weight-normal">San Francisco</td>
-                        <td class="text-sm font-weight-normal">36</td>
-     
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Haley Kennedy</td>
-                        <td class="text-sm font-weight-normal">
-                            Senior Marketing Designer
-                        </td>
-                        <td class="text-sm font-weight-normal">London</td>
-                        <td class="text-sm font-weight-normal">43</td>
-   
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Tatyana Fitzpatrick
-                        </td>
-                        <td class="text-sm font-weight-normal">
-                            Regional Director
-                        </td>
-                        <td class="text-sm font-weight-normal">London</td>
-                        <td class="text-sm font-weight-normal">19</td>
-           
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Michael Silva</td>
-                        <td class="text-sm font-weight-normal">
-                            Marketing Designer
-                        </td>
-                        <td class="text-sm font-weight-normal">London</td>
-                        <td class="text-sm font-weight-normal">66</td>
-               
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Paul Byrd</td>
-                        <td class="text-sm font-weight-normal">
-                            Chief Financial Officer (CFO)
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">64</td>
-             
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Gloria Little</td>
-                        <td class="text-sm font-weight-normal">
-                            Systems Administrator
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">59</td>
-          
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Bradley Greer</td>
-                        <td class="text-sm font-weight-normal">
-                            Software Engineer
-                        </td>
-                        <td class="text-sm font-weight-normal">London</td>
-                        <td class="text-sm font-weight-normal">41</td>
-         
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Dai Rios</td>
-                        <td class="text-sm font-weight-normal">Personnel Lead</td>
-                        <td class="text-sm font-weight-normal">Edinburgh</td>
-                        <td class="text-sm font-weight-normal">35</td>
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">
-                            Jenette Caldwell
-                        </td>
-                        <td class="text-sm font-weight-normal">
-                            Development Lead
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">30</td>
-         
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Yuri Berry</td>
-                        <td class="text-sm font-weight-normal">
-                            Chief Marketing Officer (CMO)
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">40</td>
-                  
-                    </tr>
-                    <tr>
-                        <td class="text-sm font-weight-normal">Caesar Vance</td>
-                        <td class="text-sm font-weight-normal">
-                            Pre-Sales Support
-                        </td>
-                        <td class="text-sm font-weight-normal">New York</td>
-                        <td class="text-sm font-weight-normal">40</td>
+                    @foreach ($schoolData as $index => $school)
+                        <tr>
+                            <td>{{ $index+1 }}</td>
+                            <td>{{ $school->school_name }}</td>
+                            <td>{{ $school->district_name }}</td>
+                            <td><button class="btn btn-outline-info" type="button">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
 @endsection
+
+@push('css')
+    <style>
+        table th:first-child,
+        table td:first-child {
+            width: 1%;
+            white-space: nowrap;
+            /* Prevents wrapping */
+        }
+
+        table th:last-child,
+        table td:last-child {
+            width: 15%;
+            white-space: nowrap;
+            /* Optional, depending on content */
+        }
+
+        td {
+            font-size: 0.875em;
+        }
+    </style>
+@endpush
 
 @push('scripts')
     <script>
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SchoolController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
@@ -28,6 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // School
     Route::get('/school', [SchoolController::class, 'index'])->name('school');
 });
 
