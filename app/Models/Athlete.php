83cf9c9ffdbda3eec85884a1947dd4376ce84296
hamizanamin <hamizanamin@utm.my>
2025-06-02 00:30:02 +0800

    relationship function - model coach athletes

--- app/Models/Athlete.php
+++ app/Models/Athlete.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Athlete extends Model
 {
-    protected $table = 'athlete'; // actual table name
+    protected $table      = 'athlete';    // actual table name
     protected $primaryKey = 'id_athlete'; // custom primary key
-    public $timestamps = false; // because you're not using Laravel's default timestamps
+    public $timestamps    = false;        // because you're not using Laravel's default timestamps
 
     protected $fillable = [
         'user_id',
@@ -23,4 +23,10 @@ class Athlete extends Model
         'created_at',
         'modified_on',
     ];
+
+    public function coachAthletes()
+    {
+        return $this->belongsTo(Coach::class, 'coach_id', 'id_coach');
+    }
+
 }
