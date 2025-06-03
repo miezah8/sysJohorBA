83c2a4ffb80d152b2001698b60760732bdaf03ef
miezah8 <nurulhamizah.ma@gmail.com>
2025-06-03 22:30:11 +0800

    Update logo pada views

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Johor BA Information Management System') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +18,9 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="http://127.0.0.1:8000/images/logo_pbj1.png" class="w-40 h-40 fill-current text-gray-500 mx-auto d-block" />
+                    {{--  <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                    <span class="ms-100 font-weight-bold">Johor BA Information Management System</span>
                 </a>
             </div>
 
